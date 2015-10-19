32cb70d4f3303d167522fea338ad460b2701897a
Daniel Playle <dan.playle@gmail.com>
2015-10-19 21:39:53 +0100

    Changed plot script to allow for interaction

--- plot.sh
+++ plot.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo -e "set datafile separator ','\nplot '$1' using 1:2 with lines title columnhead, '' using 1:3 with lines title columnhead, '' using 1:4 with lines title columnhead" | gnuplot --persist
+( echo -e "set datafile separator ','\nplot '$1' using 1:2 with lines title columnhead, '' using 1:3 with lines title columnhead, '' using 1:4 with lines title columnhead" && cat ) | gnuplot --persist
