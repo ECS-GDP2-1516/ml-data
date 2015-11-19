5341999956195b0c6b8efda5bfa07871ff4d419b
Daniel Playle <dan.playle@gmail.com>
2015-11-19 16:07:20 +0000

    Added utility to downsampling

--- freq-reduction-demo/make-reductions.sh
+++ freq-reduction-demo/make-reductions.sh
@@ -4,12 +4,17 @@ dir=$1
 
 while read frequency
 do
-    mkdir "$dir-${frequency}hz"
+    mkdir "$dir/reduced/${frequency}hz"
 
     ls "$dir" | grep 'csv$' |
         while read filename
         do
-            ../reduce.sh "$frequency" "$dir/$filename" >"$dir-${frequency}hz/$filename"
-            echo "Done $dir-${frequency}hz/$filename"
+            if [ ! -f "$dir/reduced/${frequency}hz/$filename" ]
+            then
+                ../reduce.sh "$frequency" "$dir/$filename" >"$dir/reduced/${frequency}hz/$filename"
+                echo "Done $dir/reduced/${frequency}hz/$filename"
+            else
+                echo "Skipping $dir/reduced/${frequency}hz/$filename, already done"
+            fi
         done
 done
